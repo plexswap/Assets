5a35e5edd2837a45e1ec279478ef0e4bf2ab14b1
Symplexia Labs <suporte@symplexia.com.br>
2023-12-18 20:11:17 -0300

    Unbelievable

---
+++ app/index.js
@@ -0,0 +1,13 @@
+/*
+import express, { Router } from "express"
+import serverless from "serverless-http"
+
+const express = require('express')
+const router = Router()
+
+const app = express()
+
+app.use(express.static(''), router)
+
+export const handler = serverless(app);
+*/
